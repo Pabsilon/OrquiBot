4ae85aaf2045fd507e9f1709213798dcbc214330
Pablo Mac-Veigh <pablomacveigh@gmail.com>
2017-07-26 00:25:43 +0200

    Initial Commit

---
+++ Config.py
@@ -0,0 +1,2 @@
+HOST_NAME = 'hostNameHere'
+API_TOKEN = 'TokenHere'
---
+++ Main.py
@@ -0,0 +1,26 @@
+from telegram.ext import Updater, CommandHandler
+import Config
+
+#Pont was here and didn't want to touch anything
+def start(bot, update):
+    update.message.reply_text('Hello Wolrd!')
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+def isItOn(bot, update):
+    response = os.system("ping -c 1 " + Config.HOST_NAME)
+    update.message.reply_text('Pinging...')
+    if response == 0:
+        update.message.reply_text('No luck mate')
+    else:
+        update.message.reply_text('Server is up!')
+
+updater = Updater(Config.API_TOKEN)
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
