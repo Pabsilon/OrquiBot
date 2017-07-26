b621a9069e775305c9ffca7a13c90abaa60ed491
Pablo Mac-Veigh <pablomacveigh@gmail.com>
2017-07-26 23:02:31 +0200

    /userLogIn works both with User and SubAdmins.

--- Modules/FileZilla.py
+++ Modules/FileZilla.py
@@ -1,5 +1,5 @@
 from xml.etree import ElementTree as ET
-from Config import XML_PATH,LOGGED_ADMINS
+from Config import XML_PATH,LOGGED_ADMINS, LOGGED_USERS
 
 def adminLogIn(bot, update, args):
     #Allows an admin from the ftp to log in.
@@ -28,3 +28,31 @@ def adminLogIn(bot, update, args):
 
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /adminLogIn Username')
+
+def userLogIn(bot, update, args):
+    #Allows a user from the ftp to log in.
+    try:
+        user = args[0]
+        if LOGGED_USERS.get(update.message.from_user.username):
+            update.message.reply_text('You\'re already logged in as ' + LOGGED_USERS.get(update.message.from_user.username))
+        elif user:
+            tree = ET.parse(XML_PATH)
+            root = tree.getroot()
+            root = root[2]
+            exists = False
+            for child in root:
+                if args[0] == child.get('Name'):
+                    exists = True
+                    options = child[2]
+                    if options.text == 'Users' or  'SubAdmin':
+                        LOGGED_USERS[update.message.from_user.username]=args[0]
+                        update.message.reply_text('Yay! ' + args[0] + ' logged as ' + update.message.from_user.username)
+                    else:
+                        update.message.reply_text('I don\'t know any users with that name...')
+            if not exists:
+                update.message.reply_text('I don\'t know any users with that name...')
+        else:
+            update.message.reply_text('XML file missing. Try updating Config.py')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /userLogIn Username')
