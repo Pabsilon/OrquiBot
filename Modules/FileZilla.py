e7cbc4beba3199d0a89e7f40e163f5c4739b706c
Pablo Mac-Veigh <pablomacveigh@gmail.com>
2017-07-27 01:02:52 +0200

    AdminLogIn and UserLogIn merged into logIn. logout implemented

--- Modules/FileZilla.py
+++ Modules/FileZilla.py
@@ -7,6 +7,8 @@ def adminLogIn(bot, update, args):
         user = args[0]
         if LOGGED_ADMINS.get(update.message.from_user.username):
             update.message.reply_text('You\'re already logged in as ' + LOGGED_ADMINS.get(update.message.from_user.username))
+        elif LOGGED_USERS.get(update.message.from_user.username):
+            update.message.reply_text('You\'re already logged in as ' + LOGGED_USERS.get(update.message.from_user.username))
         elif user:
             tree = ET.parse(XML_PATH)
             root = tree.getroot()
@@ -18,41 +20,27 @@ def adminLogIn(bot, update, args):
                     options = child[2]
                     if options.text == 'SuperUsers':
                         LOGGED_ADMINS[update.message.from_user.username]=args[0]
+                        update.message.reply_text('Welcome, Admin: ' + args[0] + ' logged as ' + update.message.from_user.username)
+                    elif options.text == 'Users' or 'SubAdmins':
+                        LOGGED_USERS[update.message.from_user.username] = args[0]
                         update.message.reply_text('Yay! ' + args[0] + ' logged as ' + update.message.from_user.username)
-                    else:
-                        update.message.reply_text('I don\'t know any admins with that name...')
+
             if not exists:
-                update.message.reply_text('I don\'t know any admins with that name...')
+                update.message.reply_text('I don\'t know anyone with that name...')
         else:
             update.message.reply_text('XML file missing. Try updating Config.py')
 
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /adminLogIn Username')
 
-def userLogIn(bot, update, args):
-    #Allows a user from the ftp to log in.
+def logOut(bot,update):
+    user = update.message.from_user.username
     try:
-        user = args[0]
-        if LOGGED_USERS.get(update.message.from_user.username):
-            update.message.reply_text('You\'re already logged in as ' + LOGGED_USERS.get(update.message.from_user.username))
-        elif user:
-            tree = ET.parse(XML_PATH)
-            root = tree.getroot()
-            root = root[2]
-            exists = False
-            for child in root:
-                if args[0] == child.get('Name'):
-                    exists = True
-                    options = child[2]
-                    if options.text == 'Users' or  'SubAdmin':
-                        LOGGED_USERS[update.message.from_user.username]=args[0]
-                        update.message.reply_text('Yay! ' + args[0] + ' logged as ' + update.message.from_user.username)
-                    else:
-                        update.message.reply_text('I don\'t know any users with that name...')
-            if not exists:
-                update.message.reply_text('I don\'t know any users with that name...')
-        else:
-            update.message.reply_text('XML file missing. Try updating Config.py')
-
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /userLogIn Username')
+        LOGGED_ADMINS.pop(user)
+    except KeyError:
+        try:
+            LOGGED_USERS.pop(user)
+        except KeyError:
+            update.message.reply_text('Wait a second... You weren\'t event logged in!')
+            return
+    update.message.reply_text('Bye bye, '+user)
