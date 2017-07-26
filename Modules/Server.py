407fb72ea8db7c00c33da717157de24ccdbe1a82
Pablo Alapont <pablo_alapont@hotmail.com>
2017-07-26 14:17:42 +0200

    /server does the same as /server help

--- Modules/Server.py
+++ Modules/Server.py
@@ -1,19 +1,18 @@
 #status reboot distress
 
 def start(bot, update,args):
-    print("I\'m starting")
     update.message.reply_text('Hello Wolrd!')
 
 def status(bot,update,args):
-    print ("I'm status")
     #status should check if diferent services are up or down
     update.message.reply_text('I\'m still here')
+
 #def distress(bot,update):
  #   update.message.reply_text('If you want to contact the admins text to @SeñorPont')
+
 def help(bot,update,args):
-    print('I\'m helping')
     update.message.reply_text("/Server manages the server, you can:\n"+
-                              "/start\tto let the bot know you are there\n"+
+                              "/server start\tto let the bot know you are there\n"+
                               "/status\tto know what services are up\n"+
                               "/help\tto print this help\n")
 
@@ -22,9 +21,9 @@ def handler(bot,update,args):
     for arg in args:
         print(arg,  end=' ')
     print("")
+    if ( len(args)==0 or args[0].lower() == 'help'):
+        help(bot,update,args)
     if (args[0].lower() == 'start'):
         start(bot,update,args)
     if (args[0].lower() == 'status'):
         status(bot,update,args)
-    if (args[0].lower() == 'help' or len(args)==0):
-        help(bot,update,args)
