c4d91034f87ff84f1b5f3ac7e84f9b07a32afe26
Pablo Mac-Veigh <pablomacveigh@gmail.com>
2017-07-28 16:47:44 +0200

    Recovering config.py (reverted from commit 5810f1f1ce877ffea1dc4fd6fae185842436b3e4)

--- Config.py
+++ Config.py
@@ -2,13 +2,16 @@ import socket
 
 HOST_NAME = 'orquishare.orquimed.es'
 
-if socket.gethostname() in ['DESKTOP-1E39VB7' , 'ALPHA']: #Ordenadores de Mac
+if socket.gethostname() == 'DESKTOP-1E39VB7' or 'ALPHA': #Ordenadores de Mac
     XML_PATH='c:\\Users\\Pabs\\Desktop\\FileZilla Server.xml'
-    API_TOKEN = '376202632:AAHFi16Wc37p1VY_mJseP5S1zdy0xw5cncU' # Orquibot
-elif socket.gethostname() in[ 'Zarya'or 'OptimusPrime']: #El ordenador de pont
+    API_TOKEN = '350174683:AAFL4yesbHgQxOhI_5LybVZV8akQIycyVuw' # ORQUITESTBOT
+if socket.gethostname() == 'Zarya': #El ordenador de pont
     XML_PATH='Ruta con doble \\ al fichero xml.'
     API_TOKEN = '436440922:AAGUFL-tiwt3g7dmX8Z7S10URD2QDwJJupU'  # NOTORQUIBOT
-
+if socket.gethostname() == 'Server':
+    print(socket.gethostname())
+    XML_PATH='c:\\Hosted\\FileZilla Server\\FileZilla Server.xml'
+    API_TOKEN = '376202632:AAHFi16Wc37p1VY_mJseP5S1zdy0xw5cncU'  # Orquibot
 
 ADMIN_GROUP = 'SuperUsers'
 USERS_GROUP = 'Users', 'SubAdmin'
