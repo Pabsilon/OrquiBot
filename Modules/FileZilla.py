2fd584f2fc137453557a9864d7920bec81a7d319
Pabsilon <pablomacveigh@gmail.com>
2018-04-01 18:31:28 +0200

    Users and admins are saved on log-in. Users and admins are deleted from the file on log-out. 2FA further tested.

--- Modules/FileZilla.py
+++ Modules/FileZilla.py
@@ -1,33 +1,38 @@
-from xml.etree import ElementTree as ET
+from xml.etree import ElementTree
 from Config import XML_PATH, ADMIN_GROUP, USERS_GROUP, USERS_FOLDER
 from Modules.Users import LOGGED_ADMINS, LOGGED_USERS
-import logging, os.path, string, random
+from Modules.PersistentStorage import saveAdmins, saveUsers
+import logging
+import os.path
+import string
+import random
 
-def adminLogIn(bot, update, args):
+
+def login(bot, update, args):
     logging.warning("Log in attempt by " + str(update.message.from_user.username))
-    #Allows an admin from the ftp to log in.
+    # Allows an admin from the ftp to log in.
 
     try:
         user = args[0]
-        alreadyLoggedIn = False;
-        #This block checks if the user is already logged in, as either User or Admin and returns a message.
-        for dict in LOGGED_ADMINS.values():
-            if user in dict:
-                alreadyLoggedIn = True
-        for dict in LOGGED_USERS.values():
-            if user in dict:
-                alreadyLoggedIn = True
+        alreadyloggedin = False
+        # This block checks if the user is already logged in, as either User or Admin and returns a message.
+        for dd in LOGGED_ADMINS.values():
+            if user in dd:
+                alreadyloggedin = True
+        for dd in LOGGED_USERS.values():
+            if user in dd:
+                alreadyloggedin = True
         if LOGGED_ADMINS.get(update.message.from_user.username):
-            #This is only if you're already logged in as an admin
+            # This is only if you're already logged in as an admin
             update.message.reply_text('You\'re already logged in as ' + LOGGED_ADMINS[update.message.from_user.username].get('name'))
         elif LOGGED_USERS.get(update.message.from_user.username):
-            #This is only if you're already logged in as a user
+            # This is only if you're already logged in as a user
             update.message.reply_text('You\'re already logged in as ' + LOGGED_USERS[update.message.from_user.username].get('name'))
-        elif alreadyLoggedIn:
-            #This only happens if another user is logged with your account (hax much?)
+        elif alreadyloggedin:
+            # This only happens if another user is logged with your account (hax much?)
             update.message.reply_text('That user is already logged on by another person. If you think this is a mistake, well.. tough luck.')
         elif user:
-            tree = ET.parse(XML_PATH)
+            tree = ElementTree.parse(XML_PATH)
             root = tree.getroot()
             root = root[2]
             exists = False
@@ -35,7 +40,7 @@ def adminLogIn(bot, update, args):
                 if args[0] == child.get('Name'):
                     exists = True
                     options = child[2]
-                    fname = USERS_FOLDER +'\\' + args[0] + "\\orclogin.txt"
+                    fname = USERS_FOLDER + '\\' + args[0] + "\\orclogin.txt"
                     if os.path.isfile(fname):
                         if options.text in ADMIN_GROUP:
                             file = open(fname, "r")
@@ -45,6 +50,7 @@ def adminLogIn(bot, update, args):
                                 os.remove(fname)
                                 LOGGED_ADMINS[update.message.from_user.username] = {'name': args[0], 'chatId': update.message.chat_id}
                                 update.message.reply_text('Welcome, Admin: ' + args[0] + ' logged as ' + update.message.from_user.username)
+                                saveAdmins()
                             else:
                                 update.message.reply_text('Auth failed. Try removing the orclogin.txt file to generate a new one')
                         elif options.text in USERS_GROUP:
@@ -55,7 +61,7 @@ def adminLogIn(bot, update, args):
                                 os.remove(fname)
                                 LOGGED_USERS[update.message.from_user.username] = {'name': args[0], 'chatId': update.message.chat_id}
                                 update.message.reply_text('Yay! ' + args[0] + ' logged as ' + update.message.from_user.username)
-
+                                saveUsers()
                     else:
                         file = open(fname, "w+")
                         text = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
@@ -71,7 +77,8 @@ def adminLogIn(bot, update, args):
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /login Username - Remember, Username is Case Sensitive')
 
-def logOut(bot,update):
+
+def logout(bot, update):
     logging.warning("Logout attempted by " + str(update.message.from_user.username))
     user = update.message.from_user.username
     try:
@@ -83,4 +90,5 @@ def logOut(bot,update):
             update.message.reply_text('Wait a second... You aren\'t even logged in!')
             return
     update.message.reply_text('Bye bye, '+user+'!')
-
+    saveUsers()
+    saveAdmins()
--- Modules/PersistentStorage.py
+++ Modules/PersistentStorage.py
@@ -1,24 +1,51 @@
-from Modules.Users import LOGGED_ADMINS
+from Modules.Users import LOGGED_ADMINS, LOGGED_USERS
 import os
 import logging
 
 def loadAdmins():
     logging.warning("Loading admins.")
-    if os.stat("admins.orc").st_size!=0:
-        file = open("admins.orc", "r")
-        line = file.readline()
-        while line != '':
-            LOGGED_ADMINS[line.rstrip("\n")]={'name': file.readline().rstrip("\n"), 'chatId' : file.readline().rstrip("\n")}
+    try:
+        if os.stat("admins.orc").st_size!=0:
+            file = open("admins.orc", "r")
             line = file.readline()
-        file.close()
-
+            while line != '':
+                LOGGED_ADMINS[line.rstrip("\n")]={'name': file.readline().rstrip("\n"), 'chatId' : file.readline().rstrip("\n")}
+                line = file.readline()
+            file.close()
+    except FileNotFoundError:
+        logging.warning("No admins previously logged in")
 
 def saveAdmins():
     logging.warning("Saving admins.")
-    file = open ("admins.orc", "w")
-    for admin in LOGGED_ADMINS:
-        file.write(admin + "\n")
+    file = open ("admins.orc", "w+")
+        for admin in LOGGED_ADMINS:
+            file.write(admin + "\n")
         file.write(LOGGED_ADMINS[admin].get('name')+"\n")
         test = LOGGED_ADMINS[admin].get('chatId')
+        file.write(str(test) + "\n")
+    file.close()
+
+
+def loadUsers():
+    logging.warning("Loading users.")
+    try:
+        if os.stat("users.orc").st_size != 0:
+            file = open("users.orc", "r")
+            line = file.readline()
+            while line != '':
+                LOGGED_USERS[line.rstrip("\n")] = {'name': file.readline().rstrip("\n"), 'chatId': file.readline().rstrip("\n")}
+                line = file.readline()
+            file.close()
+    except FileNotFoundError:
+        logging.warning("No users previously logged in")
+
+
+def saveUsers():
+    logging.warning("Saving users.")
+    file = open("users.orc", "w+")
+    for user in LOGGED_USERS:
+        file.write(user + "\n")
+        file.write(LOGGED_USERS[user].get('name') + "\n")
+        test = LOGGED_USERS[user].get('chatId')
         file.write(str(test) + "\n")
     file.close()
